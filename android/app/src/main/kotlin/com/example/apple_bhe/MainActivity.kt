a79b76e937049dbdc9b0bd4ec31f8ef9595cf655
DhanrajRateria <116026855+DhanrajRateria@users.noreply.github.com>
2023-03-04 00:16:38 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/apple_bhe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apple_bhe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/registration_screen.dart';
+import 'screens/welcome_screen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark(),
+        initialRoute: WelcomeScreen.id,
+        routes: {
+          HomeScreen.id:(context) => HomeScreen(),
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+        });
+  }
+}

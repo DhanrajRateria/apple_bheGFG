c1b7c20e07389d51b0c621ff9c0c4e5877d89199
Dhanraj Rateria <dhanrajrateria@gmail.com>
2023-03-18 13:55:52 +0530

    Adding images

--- lib/screens/information_screens/personnel.dart
+++ lib/screens/information_screens/personnel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:apple_bhe/image_buttons.dart';
+import 'package:image_picker/image_picker.dart';
 
 class Personnel extends StatefulWidget {
   static const String id = 'personnel';
@@ -9,6 +10,11 @@ class Personnel extends StatefulWidget {
 }
 
 class _PersonnelState extends State<Personnel> {
+  TextEditingController _controller1 = TextEditingController();
+  TextEditingController _controller2 = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
+  XFile? _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +29,38 @@ class _PersonnelState extends State<Personnel> {
           desc: "Founder & CEO",
         ),
       ),
+      floatingActionButton: FloatingActionButton(onPressed: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Add Person"),
+                content: Column(children: [
+                  TextField(
+                    controller: _controller1,
+                    decoration: InputDecoration(labelText: "Enter Name"),
+                  ),
+                  TextField(
+                    controller: _controller2,
+                    decoration: InputDecoration(labelText: "Enter Designation"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      final pickedFile = await ImagePicker().pickImage(
+                        source: ImageSource.gallery, // or ImageSource.camera
+                      );
+                      if (pickedFile != null) {
+                        setState(() {
+                          _image = XFile(pickedFile.path);
+                        });
+                      }
+                    },
+                    child: Text('Pick Image'),
+                  ),
+                ]),
+              );
+            });
+      }),
     );
   }
 }

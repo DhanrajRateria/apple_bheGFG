a829faa28a51c0e0cb2535d459048e3e664bf912
Dhanraj Rateria <dhanrajrateria@gmail.com>
2023-03-21 23:44:49 +0530

    UI improved

--- lib/screens/information_screens/location.dart
+++ lib/screens/information_screens/location.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart' as pkgLocation;
+import 'package:geolocator/geolocator.dart';
 
 class Location extends StatefulWidget {
   static const String id = 'location_screen';
@@ -8,8 +11,42 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+  late GoogleMapController _mapController;
+  pkgLocation.LocationData? _currentLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    final location = pkgLocation.Location();
+    final status = await location.requestPermission();
+    if (status == pkgLocation.PermissionStatus.granted) {
+      final currentLocation = await location.getLocation();
+      setState(() {
+        _currentLocation = currentLocation;
+      });
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: _currentLocation == null
+          ? Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(
+                  _currentLocation!.latitude!,
+                  _currentLocation!.longitude!,
+                ),
+                zoom: 15,
+              ),
+              onMapCreated: (controller) => _mapController = controller,
+              myLocationEnabled: true,
+            ),
+    );
   }
 }

1b50ad3a3fb94ba535cca6b3690f335546ae7214
Dhanraj Rateria <dhanrajrateria@gmail.com>
2023-03-27 12:07:06 +0530

    Completing the UI

--- lib/screens/information_screens/financials.dart
+++ lib/screens/information_screens/financials.dart
@@ -8,8 +8,33 @@ class Financials extends StatefulWidget {
 }
 
 class _FinancialsState extends State<Financials> {
+  int currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        selectedIndex: currentPageIndex,
+        destinations: [
+          NavigationDestination(
+            icon: Icon(Icons.person),
+            label: 'General',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.account_balance),
+            label: 'Financials',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.admin_panel_settings),
+            icon: Icon(Icons.bookmark_border),
+            label: 'Admin',
+          ),
+        ],
+      ),
+    );
   }
 }

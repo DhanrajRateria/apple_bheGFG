bd5495a7c7b5576978586762e4c9cc4441ac72fc
Dhanraj Rateria <dhanrajrateria@gmail.com>
2023-05-19 15:26:25 +0530

    Final change

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -30,87 +30,65 @@ class _HomeScreenState extends State<HomeScreen> {
             padding: EdgeInsets.all(16.0),
             child: Column(
               children: [
-                Text(
-                  "Welcome!",
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 16.0),
                 Image.asset(
                   'images/logistic.png',
                   height: 200.0,
                 ),
+                Text('Welcome',
+                    style:
+                        TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold))
               ],
             ),
           ),
           Expanded(
-            child: SingleChildScrollView(
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        HomeButton(
-                          title: "Inventory",
-                          imagename: "inventory",
-                          onPressed: () {
-                            Navigator.pushNamed(context, InventoryScreen.id);
-                          },
-                        ),
-                        HomeButton(
-                          title: "Ledger",
-                          imagename: "ledger",
-                          onPressed: () {
-                            Navigator.pushNamed(context, LedgerScreen.id);
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16.0),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        HomeButton(
-                          title: "Orders",
-                          imagename: "orders",
-                          onPressed: () {
-                            Navigator.pushNamed(context, OrderScreen.id);
-                          },
-                        ),
-                        HomeButton(
-                          title: "Track",
-                          imagename: "location",
-                          onPressed: () {
-                            Navigator.pushNamed(context, LocationScreen.id);
-                          },
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 16.0),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        HomeButton(
-                          title: "Personnel",
-                          imagename: "personnel",
-                          onPressed: () {
-                            Navigator.pushNamed(context, Personnel.id);
-                          },
-                        ),
-                        HomeButton(
-                          title: "Company Info",
-                          imagename: "financials",
-                          onPressed: () {
-                            Navigator.pushNamed(context, Financials.id);
-                          },
-                        ),
-                      ],
-                    ),
-                  ],
+            child: GridView.count(
+              crossAxisCount: 2,
+              childAspectRatio: 1.0,
+              padding: EdgeInsets.all(16.0),
+              children: [
+                HomeButton(
+                  title: "Inventory",
+                  imagename: "inventory",
+                  onPressed: () {
+                    Navigator.pushNamed(context, InventoryScreen.id);
+                  },
                 ),
-              ),
+                HomeButton(
+                  title: "Ledger",
+                  imagename: "ledger",
+                  onPressed: () {
+                    Navigator.pushNamed(context, LedgerScreen.id);
+                  },
+                ),
+                HomeButton(
+                  title: "Orders",
+                  imagename: "orders",
+                  onPressed: () {
+                    Navigator.pushNamed(context, OrderScreen.id);
+                  },
+                ),
+                HomeButton(
+                  title: "Track",
+                  imagename: "location",
+                  onPressed: () {
+                    Navigator.pushNamed(context, LocationScreen.id);
+                  },
+                ),
+                HomeButton(
+                  title: "Personnel",
+                  imagename: "personnel",
+                  onPressed: () {
+                    Navigator.pushNamed(context, Personnel.id);
+                  },
+                ),
+                HomeButton(
+                  title: "Company Info",
+                  imagename: "financials",
+                  onPressed: () {
+                    Navigator.pushNamed(context, Financials.id);
+                  },
+                ),
+              ],
             ),
           ),
         ],
